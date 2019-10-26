c6828717ae30933baf953aeebe4a87c4a0338124
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-10-26 05:11:27 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,4 +4,4 @@ def meal_choice(veg1 = "broccoli", veg2 = "macaroni", protein = "meat")
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
   p
 end
-meal_choice("broccoli", "macaroni", "meat")
+#meal_choice("broccoli", "macaroni", "meat")
