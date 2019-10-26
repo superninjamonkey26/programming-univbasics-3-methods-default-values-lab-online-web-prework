e0942a6ab4a234b72c044b1e900dd93616375abd
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-10-26 04:49:09 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,5 @@
 # Your code here
 def meal_choice(w, y, protein ="meat")
-  p "What a nutritious meal!"
-  puts "A plate of #{protein} with #{w} and #{y}."
+  puts "What a nutritious meal!"
+  p "A plate of #{protein} with #{w} and #{y}."
 end
